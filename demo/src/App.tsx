eddf3458d21eba90fbd2b3ab8e4fd08e9c346be7
Rob Fairclough <rob@silverchip.com>
2020-01-19 19:30:09 +0000

    demo progress

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -4,8 +4,18 @@ import './App.css';
 import { TextInput } from './silverforms';
 import { HandleChangeFunc } from './silverforms/types/shared';
 
+type FormState = {
+    textInput: string,
+    numberInput?: number,
+    [key: string]: string | number | undefined,
+};
+
 const App: React.FC = () => {
-    const [form, onChange] = useState({ textInput: '' });
+    const initialFormState: FormState = {
+        textInput: '',
+        numberInput: 0,
+    };
+    const [form, onChange] = useState(initialFormState);
 
     const handleChange: HandleChangeFunc<string> = (name, value) => {
         onChange({ ...form, [name]: value });
@@ -15,21 +25,31 @@ const App: React.FC = () => {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <TextInput name="textInput" value={form.textInput} handleChange={handleChange} />
                 <a
                     className="App-link"
-                    href="https://reactjs.org"
+                    href="https://github.com/silverchiplabs/silverforms"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    SilverForms
                 </a>
+                <p>Below is a form demonstrating each available form input.</p>
             </header>
+            <div className="form-input">
+                <TextInput name="textInput" value={form.textInput} handleChange={handleChange} />
+            </div>
+
+            <div className="form-output">{getFormValues()}</div>
         </div>
     );
+
+    function getFormValues(): JSX.Element[] {
+        return Object.keys(form).map(key => (
+            <p>
+                {key}: {form[key]}
+            </p>
+        ));
+    }
 };
 
 export default App;
